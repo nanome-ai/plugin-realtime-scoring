54297b174a18f86ef06fd3e9a6a93569f12b8c3b
Max Howard <max@nanome.ai>
2020-01-16 14:25:10 -0800

    simplified local deploy

--- docker/local_deploy.sh
+++ docker/local_deploy.sh
@@ -1,9 +1,7 @@
-if [ "$(docker ps -q -f name=realtime_scoring)" != "" ]; then
-    if [ "$(docker ps -aq -f status=exited -f name=realtime_scoring)" != "" ]; then
+if [ "$(docker ps -aq -f name=realtime_scoring)" != "" ]; then
         # cleanup
-        docker rm realtime_scoring
-    fi
-    docker rm -f realtime_scoring
+        echo "removing exited container"
+        docker rm -f realtime_scoring
 fi
 
 if [ "$1" != "" ]; then
