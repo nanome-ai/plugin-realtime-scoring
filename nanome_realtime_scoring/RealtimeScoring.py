f6b827e87826db01ab37de4cdd90a06cb2d05a48
Mike Rosengrant <mjrosengrant@users.noreply.github.com>
2021-07-21 15:02:43 -0400

    Update RealtimeScoring.py

--- nanome_realtime_scoring/RealtimeScoring.py
+++ nanome_realtime_scoring/RealtimeScoring.py
@@ -329,7 +329,7 @@ class RealtimeScoring(nanome.PluginInstance):
         self._streams_ready = False
 
         scales = []
-        if self._spheres:
+        if self._spheres != []:
             if streams_ready:
                 # make all spheres transparent so user can't see deletion process
                 for i in range(len(self._spheres)):
