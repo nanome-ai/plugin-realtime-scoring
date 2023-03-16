e39e85126f1d2484f0357008111563ad5a2edf31
Mike Rosengrant <mike.rosengrant@nanome.ai>
2023-03-16 16:20:38 -0400

    space

--- scoring_schema.py
+++ scoring_schema.py
@@ -3,6 +3,7 @@ from marshmallow import Schema, fields
 
 class LigandScoringSchema(Schema):
     """Output schema that custom scoring functions must return."""
+
     complex_index = fields.Integer(required=False)
     aggregate_scores = fields.List(
         fields.Dict(keys=fields.Str(), values=fields.Float()),
